6f333fcc762aaa9d047e2bd34ad40dcfe54873ac
Nguyennanghung <nguyennanghung20@gmail.com>
2021-02-26 10:05:21 +0700

    Add queues task

--- src/Network.java
+++ src/Network.java
@@ -1,19 +1,28 @@
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class Network {
+    private int numTaskTotal = 0;
     private int numShelf = Config.getInstance().getAsInteger("Shelf_quantity");
     private int numManubot = Config.getInstance().getAsInteger("ManuBot_quantity");
     private int numCharger = Config.getInstance().getAsInteger("Charger_quantity");
     private int factorySize = Config.getInstance().getAsInteger("Factory_size");
-    private String Door_xcord = Config.getInstance().getAsString("Door_xcord");
-    private String Door_ycord = Config.getInstance().getAsString("Door_ycord");
+    private String Gate_xcord_in = Config.getInstance().getAsString("Gate_xcord_in");
+    private String Gate_ycord_in = Config.getInstance().getAsString("Gate_ycord_in");
+    private String Gate_xcord_out = Config.getInstance().getAsString("Gate_xcord_out");
+    private String Gate_ycord_out = Config.getInstance().getAsString("Gate_ycord_out");
 
+    // Objects section
     private List<TaskShelf> ShelfList = new ArrayList<>();
     private List<ManuBot> ManuList = new ArrayList<>();
-    private List<point> DoorList = new ArrayList<>();
+    private List<Gate> GateInList = new ArrayList<>();
+    private List<Gate> GateOutList = new ArrayList<>();
 
+    //  Queues section
+    private List<Task> ArrivalTaskQueue = new ArrayList<>();
+    private List<Task> ActiveTaskQueue = new ArrayList<>();
+
+    // Objects interact section
     public void insertShelfList(TaskShelf ts){
         this.ShelfList.add(ts);
     }
@@ -22,25 +31,75 @@ public class Network {
         this.ManuList.add(mb);
     }
 
-    public void insertDoorList(point door){
-        this.DoorList.add(door);
+    public void insertGateInList(Gate gt){
+        this.GateInList.add(gt);
     }
 
+    public void insertGateOutList(Gate gt){ this.GateOutList.add(gt); }
+
+    // Queues interact section
+    public void insertArrivalTaskQueue(Task nt){
+        this.ArrivalTaskQueue.add(nt);
+    }
+
+    public void insertActiveTaskQueue(Task nt){
+        this.ActiveTaskQueue.add(nt);
+    }
+
+    public boolean isNewTask(){
+        return this.ArrivalTaskQueue.isEmpty();
+    }
+
+    public boolean isRequesting(){
+        return this.ActiveTaskQueue.isEmpty();
+    }
+
+    // Get task that is active by insert its ID
+    public Task getActiveTask(int ID){
+        for (Task i: this.ActiveTaskQueue){
+            if (i.getID() == ID){
+                return i;
+            }
+        }
+        return null;
+    }
+
+    // Get task that is active by insert its ID
+    public Task getArrivalTask(int ID){
+        for (Task i: this.ArrivalTaskQueue){
+            if (i.getID() == ID){
+                return i;
+            }
+        }
+        return null;
+    }
+
+    // ult
     public int getFactorySize() {
         return factorySize;
     }
 
+    public int getTaskID(){
+        this.numTaskTotal += 1;
+        return this.numTaskTotal;
+    }
+
+    // Constructor
     public Network(){
-        // Initialize doors
-        String[] doorX = Door_xcord.split(";");
-        String[] doorY = Door_ycord.split(";");
-        for (int i = 0; i < 2; i++){
-            double valueX = Double.parseDouble(doorX[i]);
-            for (int j = 0; j < 2; j++){
-                double valueY = Double.parseDouble(doorY[i]);
-                point newdoor = new point(valueX, valueY);
-                insertDoorList(newdoor);
-            }
+        // Initialize gates
+        String[] GateInX = Gate_xcord_in.split(";");
+        String[] GateInY = Gate_ycord_in.split(";");
+        String[] GateOutX = Gate_xcord_out.split(";");
+        String[] GateOutY = Gate_ycord_out.split(";");
+        for (int i = 0; i < GateInX.length; i++){
+            point X = new point(Double.parseDouble(GateInX[i]), Double.parseDouble(GateInY[i]));
+            Gate gt = new Gate(X, "In");
+            insertGateInList(gt);
+        }
+        for (int i =0; i < GateOutX.length; i++){
+            point X = new point(Double.parseDouble(GateOutX[i]), Double.parseDouble(GateOutY[i]));
+            Gate gt = new Gate(X, "Out");
+            insertGateOutList(gt);
         }
 
         // Initialize shelves and setting ids
