5736f247f0c3519274e6d8d4a3687f3d868685b9
Nguyennanghung <nguyennanghung20@gmail.com>
2021-02-26 10:05:44 +0700

    Add arrival intervals

--- src/Task.java
+++ src/Task.java
@@ -1,5 +1,26 @@
+import java.util.Random;
+
 public class Task {
     private int ID;
+    private double activateTime;
+    private String rangeRandom = Config.getInstance().getAsString("Task_active_range");
+    private String[] bound = rangeRandom.split(";");
+    private double lowerBound = Double.parseDouble(bound[0]);
+    private double ranging = Double.parseDouble(bound[1]);
+
+    public double getLowerBound() {
+        return lowerBound;
+    }
+
+    public double getRanging() {
+        return ranging;
+    }
+
+    public void setActivateTime(double now) {
+        Random rand = new Random();
+        double deltaT = rand.nextGaussian()*getRanging()+getLowerBound(); // this give a number ranging from 300 to 400
+        this.activateTime = now + deltaT;
+    }
 
     public void setID(int ID) {
         this.ID = ID;
@@ -10,7 +31,8 @@ public class Task {
     }
 
     // Constructor
-    public Task(int ID){
+    public Task(int ID, double timeNow){
         setID(ID);
+        setActivateTime(timeNow);
     }
 }
