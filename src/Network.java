3416816637f677ea7b77327d06ced5405e48669b
Nguyennanghung <Nguyennanghung20@gmail.com>
2021-04-22 12:18:50 +0700

    Move getAutoBotFromXTY to ComputingCenter

--- src/Network.java
+++ src/Network.java
@@ -131,66 +131,6 @@ public class Network {
         return this.numTaskTotal;
     }
 
-    /**
-     * @author Le Minh An
-     * function choose the autoBot to get the new occured task
-     * @param PointX: location of the packet
-     * @param PointY: destination of the packet
-     * @return id of the chosen autoBot
-     */
-    public int getAutoBotFromXTY(point PointX, point PointY){
-        int chooseID1 = -1;             // The uncharged manubot can go to gate
-        int chooseID2 = -1;             // The charging manubot can complete task
-        int chooseID3 = -1;             // The charging manubot can go to gate
-        double minEstimateTime1 = 100;
-        double minEstimateTime2 = 100;
-        double minEstimateTime3 = 100;
-        for(ManuBot mb: this.ManuList) {
-            if (!mb.workList.isEmpty()){
-//                System.out.println(mb.getId());
-                continue;
-            }
-            double lengthTX = mb.getLocationNow().getLength(PointX)*1.302;          // length from current manubot's location to Gate
-            double lengthXTY = PointX.getLength(PointY)*1.302;                  // length from gate to shelf
-            double estimateTimeX = lengthTX / mb.getSpeed();                             // estimate time to Gate
-            double estimateTimeY = lengthXTY / mb.getSpeed();
-//            System.out.println("1.TX: " + lengthTX);
-//            System.out.println("XTY: " + lengthXTY);
-//            System.out.println("EX: " + estimateTimeX);
-//            System.out.println("EY: " + estimateTimeY);
-//            System.out.println(mb.getResEnergy());
-//            System.out.println(((estimateTimeX + estimateTimeY) * mb.getERperSec()));
-            if (mb.getResEnergy() >= (estimateTimeX + estimateTimeY) * mb.getERperSec()) {           // Manubot can take task and complete mission
-                if (mb.getChargingTimeLeft() == 0.0){
-                    return mb.getId();
-                }
-                if (minEstimateTime2 > estimateTimeX + estimateTimeY) {
-                    chooseID2 = mb.getId();
-                    minEstimateTime2 = estimateTimeX + estimateTimeY;
-                }
-            }
-            if (mb.getResEnergy() >= (estimateTimeX)*mb.getERperSec()){
-                if (mb.getChargingTimeLeft() == 0){
-                    if (minEstimateTime1 < estimateTimeX){
-                        chooseID1 = mb.getId();
-                        minEstimateTime1 = estimateTimeX;
-                    }
-                }
-                else if (minEstimateTime3 > estimateTimeX) {
-                    chooseID3 = mb.getId();
-                    minEstimateTime3 = estimateTimeX;
-                }
-            }
-        }
-        if (chooseID1 != -1)
-            return chooseID1;
-        if (chooseID2 != -1)
-            return chooseID2;
-        return chooseID3;
-
-    }
-
-
     // Constructor
     public Network(){
         // Initialize Queues
@@ -271,7 +211,7 @@ public class Network {
                         break;
                     }
                 }
-                int AutoBotID = net.getAutoBotFromXTY(tks.getLocationNow(), tks.getShelfLocation());
+                int AutoBotID = brain.getAutoBotFromXTY(net, tks.getLocationNow(), tks.getShelfLocation());
                 ManuBot mb = net.ManuList.get(1);
                 mb.workList.add(tks);
                 System.out.println("Assigned task id{" + tks.getID() +"} to AutoBot id {" + mb.getId() + "}");
