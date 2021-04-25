4e6dcd8b4d4ba6d9bebdd354e4e7fc23ea8da7f6
Nguyennanghung <Nguyennanghung20@gmail.com>
2021-04-25 19:14:31 +0700

    Create object ManuObject

---
+++ src/ManuObject.java
@@ -0,0 +1,20 @@
+public class ManuObject {
+    protected int objectId;
+    protected point location;
+
+    public int getObjectId() {
+        return objectId;
+    }
+
+    public void setObjectId(int objectId) {
+        this.objectId = objectId;
+    }
+
+    public point getLocation() {
+        return location;
+    }
+
+    public void setLocation(point location) {
+        this.location = location;
+    }
+}
